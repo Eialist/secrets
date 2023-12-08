f19d035c07fb8ac3c83a4a2a4ed88d85a2f9abee
Eialist <elsinoree@gmail.com>
2023-12-08 14:21:44 +0100

    trying stuff

--- src/main.jsx
+++ src/main.jsx
@@ -2,22 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { HashRouter, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import JaPage from './pages/JaPage';
 import ParallaxPage from './pages/ParallaxPage';
 
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    basename: "/secrets/",
+    path: "/secrets",
     element: <App />,
     children: [
       {
-        path: "/ja", 
+        path: "/secrets/ja", 
         element: <JaPage />
       },
       {
-        path: "/parallax",
+        path: "/secrets/parallax",
         element: <ParallaxPage />
       }
     ]
@@ -27,6 +28,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter router={router} />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
