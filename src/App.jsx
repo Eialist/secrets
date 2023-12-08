69c34226fa982b97e703240e6cab1e3d3e4c784e
Eialist <elsinoree@gmail.com>
2023-12-08 12:09:06 +0100

    checking

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 
 import "./App.css";
 import { Outlet } from "react-router-dom";
+import JaPage from "./pages/JaPage";
 
 function App() {
   return (
     <>
+    <JaPage />
     <Outlet />
     </>
   );
